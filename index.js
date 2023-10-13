b58615d418cc6836131f892354b51613aa63dcce
moradi-63 <mostafa.moradi63.it.com>
2023-10-13 23:37:56 +0330

    j10

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const save_button = document.querySelector("#save-btn");
 const input_title = document.querySelector("#title");
 const list = document.querySelector(".list");
 
-const todo_list = []
+let todo_list = []
 
-function mak_item(title){
+function RenderItem(title){
     const item = document.createElement("div");
-      item.classList.add("item")
+      item.classList.add("form")
      
       const checkbox = document.createElement("input")
       checkbox.setAttribute("type" , "checkbox")
@@ -25,34 +25,42 @@ function mak_item(title){
 function clear_input(){
     input_title.value = "";
 }
+function RenderList(){
+    for (let i = 0 ; i< todo_list.length ; i++){
+        const title = todo_list[i]
+        RenderItem(title)
+}
+}
 function syncSrorage(title){
+    
     todo_list.push(title)
     const next_list = JSON.stringify(todo_list)
     localStorage.setItem("my_list", next_list) 
 }
-save_button.addEventListener("click" , () => {
-    const val = input_title.value 
-    if (val===""){
-        alert("you should input no null")
-    }else{
-      syncSrorage(val)
-      mak_item(val)
-      clear_input()
+function load_from_storage(){
+    const ListFromStorage = JSON.parse(localStorage.getItem("my_list")) || []
+    todo_list=ListFromStorage
+    RenderList()
 
-
-      //console.log(todo_list)
-
-      //const my_collection = JSON.stringify(todo_list)
-     // console.log(my_collection)
     }
-
-
-});
-
-const previouse_list = JSON.parse(localStorage.getItem("my_list"))   
-console.log(previouse_list)
-
-for (let i = 0 ; i< previouse_list.length ; i++){
-    const title = previouse_list[i]
-    mak_item(title)
+function Events(){
+    save_button.addEventListener("click" , () => {
+        const val = input_title.value 
+        if (val===""){
+            alert("you should input no null")
+        }else{
+          syncSrorage(val)
+          RenderItem(val)
+          clear_input()
+    
+        }
+    })
 }
+
+function init(){
+    load_from_storage()
+    Events()
+
+}
+
+init()
